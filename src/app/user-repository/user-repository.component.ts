e8965b59395005da14b37fd0d6524d02ea8eea18
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-13 10:58:05 +0300

    added getRepository method

--- src/app/user-repository/user-repository.component.ts
+++ src/app/user-repository/user-repository.component.ts
@@ -1,15 +1,24 @@
+import { GithubService } from '../github-github/github-request.service';
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
-  selector: 'app-user-repository',
-  templateUrl: './user-repository.component.html',
-  styleUrls: ['./user-repository.component.css']
+  selector: 'app-user-repo',
+  templateUrl: './user-repo.component.html',
+  styleUrls: ['./user-repo.component.css']
 })
-export class UserRepositoryComponent implements OnInit {
+export class UserRepoComponent implements OnInit {
+  username: string;
+  repo: any;
+  repoService: any;
+  constructor(_repoService: GithubService) { }
+  getRepo() {
+    this.repoService.updateUserName(this.username);
+    this.repoService.getRepo();
+    this.repo = this.repoService.repo;
+  }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
