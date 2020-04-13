17b971dbeb4ce5bf0a697e671be663fc22c952f2
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-13 13:59:52 +0300

    installed bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
+
 import { User } from './user';
+import { ProfileComponent } from './profile/profile.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserRepositoryComponent } from './user-repository/user-repository.component';
-import { GithubFormComponent } from './github-form/github-form.component';
 
 
 
@@ -16,7 +17,7 @@ const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: 'repo', component: UserRepositoryComponent },
   { path: 'user', component: UserProfileComponent },
-  { path: 'profile', component:ProfileComponent},
+  { path: "profile", component: ProfileComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,36 @@
+
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 import { ProfileComponent } from './profile/profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GithubFormComponent } from './github-form/github-form.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserRepositoryComponent } from './user-repository/user-repository.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
     HomeComponent,
-    ProfileComponent
+    AboutComponent,
+    ProfileComponent,
+    NavbarComponent,
+    GithubFormComponent,
+    UserProfileComponent,
+    UserRepositoryComponent,
+    FormsModule
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
