64ebef5b98383a7c39871166aea5d42cf1fdb05f
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-13 10:49:10 +0300

    added username,profile and getprofile method

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,15 +1,20 @@
+import { GithubService } from '../github-github/github-request.service';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+username :string;
+profile:any;
+  constructor( private userService:GithubService) { }
+getProfile(){
+  this.userService.updateUseName(this.username);
+  this.userService.getProfileInfo();
+  this.profile = this.userService.user;
+}
+  ngOnInit() {
   }
 
 }
