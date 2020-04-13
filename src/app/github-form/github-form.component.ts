fc7bc78dd31c4e2c817b72d0de0a264c87d96c02
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-13 10:31:50 +0300

    added event emmiter

--- src/app/github-form/github-form.component.ts
+++ src/app/github-form/github-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-github-form',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github-form.component.css']
 })
 export class GithubFormComponent implements OnInit {
-
+  @Output() emitSearch = new EventEmitter<any>()
+  searchTerm:string;
   constructor() { }
+  search(){
+    this.emitSearch.emit(this.searchTerm)
+  }
 
-  ngOnInit(): void {
+ 
+
+  ngOnInit() {
   }
 
 }
